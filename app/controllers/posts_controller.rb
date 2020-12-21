6e1dcbba89865e8bb60f700f9c46ff593f673cef
lionjove <33174402+lionjove@users.noreply.github.com>
2020-12-22 00:49:46 +0700

    Fix server drops

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,14 +8,26 @@ class PostsController < ApplicationController
   end
 
   def show
+    unless Post.exists?(params[:id])
+      render file: Rails.root.join('/public/404.html'), status: :not_found
+      return
+    end
     @post = Post.find(params[:id])
   end
 
   def edit
+    unless Post.exists?(params[:id])
+      render file: Rails.root.join('/public/404.html'), status: :not_found
+      return
+    end
     @post = Post.find(params[:id])
   end
 
   def update
+    unless Post.exists?(params[:id])
+      render file: Rails.root.join('/public/404.html'), status: :not_found
+      return
+    end
     @post = Post.find(params[:id])
 
     if @post.update(post_params)
