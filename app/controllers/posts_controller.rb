a5c4c72149951eee632df20a27578a86fc29efeb
LiquidonRF <vitaliy199811@gmail.com>
2020-12-22 00:28:22 +0700

    Add delede and edit post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,13 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+
+    @post.destroy
+    redirect_to posts_path
+  end
+
   def create
     @post = Post.new(post_params)
 
