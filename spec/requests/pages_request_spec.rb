2c24df1e07fa48de840b262ae67132d47e75950d
lionjove <33174402+lionjove@users.noreply.github.com>
2020-12-21 23:00:45 +0700

    Added simple test for about page

--- spec/requests/pages_request_spec.rb
+++ spec/requests/pages_request_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Pages', type: :request do
+  describe 'GET /about' do
+    it 'returns http success' do
+      get '/about'
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
