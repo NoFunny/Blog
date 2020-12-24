66fec1a503a69d37c0224d57c21ef59d09720a2a
LiquidonRF <vitaliy199811@gmail.com>
2020-12-24 18:38:39 +0700

    Edit comments

--- db/migrate/20201221173725_create_comments.rb
+++ db/migrate/20201221173725_create_comments.rb
@@ -3,7 +3,8 @@ class CreateComments < ActiveRecord::Migration[6.0]
     create_table :comments do |t|
       t.string :username
       t.text :body
-      t.references :post, null: false, foreign_key: true
+      t.integer :commentable_id
+      t.string :commentable_type
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_24_092140) do
+ActiveRecord::Schema.define(version: 2020_12_24_111808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,10 +18,11 @@ ActiveRecord::Schema.define(version: 2020_12_24_092140) do
   create_table "comments", force: :cascade do |t|
     t.string "username"
     t.text "body"
-    t.bigint "post_id", null: false
+    t.integer "commentable_id"
+    t.string "commentable_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["commentable_id", "commentable_type"], name: "index_comments_on_commentable_id_and_commentable_type"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -42,5 +43,4 @@ ActiveRecord::Schema.define(version: 2020_12_24_092140) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key "comments", "posts"
 end
