bbb374e885d4c738f023c8f38a511032bd277547
NoFunny <chunikhin_1999@mail.ru>
2020-12-24 18:32:43 +0700

    Update front

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,17 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
+    # @comments.username = current_user.name
+    # p comment_params
     @comment = @post.comments.create(comment_params)
+    # p '123123123' + comments.to_s
     redirect_to post_path(@post)
   end
 
   private
 
   def comment_params
+    # name = current_user.name
     params.require(:comment).permit(:username, :body)
   end
 end
