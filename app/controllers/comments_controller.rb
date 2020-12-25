55e7d12a82be158c96180affb1b31eb02a43a4ae
NoFunny <chunikhin_1999@mail.ru>
2020-12-25 15:47:20 +0700

    Add edit comment support

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,4 +11,24 @@ class CommentsController < ApplicationController
     @comment.destroy
     redirect_to post_path(@post)
   end
+
+  def edit
+    return render file: Rails.root.join('/public/404.html'), status: :not_found unless Post.exists?(params[:post_id]) && Post.find(params[:post_id]).comments.exists?(params[:id])
+
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def update
+    return render file: Rails.root.join('/public/404.html'), status: :not_found unless Post.exists?(params[:post_id]) && Post.find(params[:post_id]).comments.exists?(params[:id])
+
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.update(comment_params) ? (redirect_to @post) : (render 'edit')
+  end
+    private
+
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,13 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
       log_in user
       remember user
+      flash[:success] = 'Welcome'
       redirect_to user
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email or password combination'
       render 'new'
     end
   end
