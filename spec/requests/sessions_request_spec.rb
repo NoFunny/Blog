f5a8f773e7591e6d678328eac68444a48ea3e74c
lionjove <33174402+lionjove@users.noreply.github.com>
2020-12-27 22:14:35 +0700

    added test for login

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe 'Sessions', type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe 'login' do
+    subject(:login) { post '/login', params: { session: { email: 'qwerty@mail.ru', password: '123456789' } } }
+
+    it 'succesfull login' do
+      post '/users',
+           params: { user: { name: 'someValidName', email: 'qwerty@mail.ru', password: '123456789',
+                             password_confirmation: '123456789' } }
+
+      redirect_path = "/users/#{User.where(email: 'qwerty@mail.ru')[0].id}"
+
+      expect(:login).to redirect_to(redirect_path)
+    end
+  end
 end
