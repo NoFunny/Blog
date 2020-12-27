a5f2a722a3baf3b9ff0183bc48462f87baa4f194
lionjove <33174402+lionjove@users.noreply.github.com>
2020-12-27 21:26:15 +0700

    tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,49 @@ RSpec.describe User, type: :model do
 
       expect(test_user).to be_valid
     end
+
+    it 'invalid user name' do
+      expect do
+        test_user = FactoryBot.create(:user, name: '123456789101112131415161718192021222324252627282930', )
+      end.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'invalid user mail' do
+      expect do
+        test_user = FactoryBot.create(:user, email: 'dfgbhjdf', )
+      end.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'invalid user password' do
+      expect do
+        test_user = FactoryBot.create(:user, password: '1', )
+      end.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'check for admin' do
+      test_user = FactoryBot.create(:user)
+
+      expect(test_user.admin?).to equal(false)
+    end
+  end
+
+  describe 'remember' do
+    it 'valid user' do
+      test_user = FactoryBot.create(:user)
+
+      test_user.remember
+
+      expect(test_user.authenticated?(test_user.remember_token)).to equal(true)
+    end
+
+    it 'forget user' do
+      test_user = FactoryBot.create(:user)
+
+      test_user.remember
+
+      test_user.forget
+
+      expect(test_user.authenticated?(test_user.remember_token)).to equal(false)
+    end
   end
 end
