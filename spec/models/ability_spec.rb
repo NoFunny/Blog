37ddf1e1e5d4c0fd7fb19f939a9328c56b7b12f5
lionjove <33174402+lionjove@users.noreply.github.com>
2020-12-27 21:39:29 +0700

    test for ability

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Ability, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'normal user' do
+    it 'valid user' do
+      test_user = FactoryBot.create(:user)
+
+      ability = described_class.new(test_user)
+
+      expect(ability.can?(:manage, :all)).to equal(false)
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,19 +10,19 @@ RSpec.describe User, type: :model do
 
     it 'invalid user name' do
       expect do
-        test_user = FactoryBot.create(:user, name: '123456789101112131415161718192021222324252627282930', )
+        FactoryBot.create(:user, name: '123456789101112131415161718192021222324252627282930')
       end.to raise_error(ActiveRecord::RecordInvalid)
     end
 
     it 'invalid user mail' do
       expect do
-        test_user = FactoryBot.create(:user, email: 'dfgbhjdf', )
+        FactoryBot.create(:user, email: 'dfgbhjdf')
       end.to raise_error(ActiveRecord::RecordInvalid)
     end
 
     it 'invalid user password' do
       expect do
-        test_user = FactoryBot.create(:user, password: '1', )
+        FactoryBot.create(:user, password: '1')
       end.to raise_error(ActiveRecord::RecordInvalid)
     end
 
