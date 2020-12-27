552c29052970fe11c982df61636b645232caca0b
lionjove <33174402+lionjove@users.noreply.github.com>
2020-12-27 19:11:43 +0700

    rubocop fixes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,14 +5,14 @@ class Ability
     # for any visitor or user
     can :read, :all
 
-    if user
-      if user.admin?
-        # admins can do any action on any model or action
-        can :manage, :all
-      else
-        # regular users can read all content
-        can :read, :all
-      end
+    return unless user
+
+    if user.admin?
+      # admins can do any action on any model or action
+      can :manage, :all
+    else
+      # regular users can read all content
+      can :read, :all
     end
   end
 end
