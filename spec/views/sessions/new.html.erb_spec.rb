d8253bedff54a3bcd2a469717df2a017ba4410e8
lionjove <33174402+lionjove@users.noreply.github.com>
2020-12-27 22:18:16 +0700

    Update new.html.erb_spec.rb

--- spec/views/sessions/new.html.erb_spec.rb
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -1,10 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe 'sessions/new.html.erb', type: :view do
-  describe "GET #new" do
-    it "render customer partial" do
-      get :signup
-      expect(response).to render_template new_user_path
-    end
-  end
 end
